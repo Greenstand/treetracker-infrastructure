3faf5a964351c499542ed3a88f88d411b9941aae
Matthew Kornfield <mckornfield@gmail.com>
2019-03-30 21:24:04 +0000

    update bash script

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+  echo "Branch name is required as first argument"
+  exit 1
+fi
 BRANCH=$1
-SRC_DIR="src"
+DIR=$(dirname "$0")
+SRC_DIR="${DIR}/src"
 MOBILE_API_DIR=$SRC_DIR"/mobile-api"
 MOBILE_API_CONTAINER_NAME="treetracker-mobile-api"
 TIMESTAMP=`date +"%s"`
 
-\rm -Rf $MOBILE_API_DIR
+rm -Rf $MOBILE_API_DIR
 git clone --depth 1 git@github.com:Greenstand/treetracker-mobile-api.git -b $BRANCH $MOBILE_API_DIR 
 cd $MOBILE_API_DIR
 git config advice.detachedHead false

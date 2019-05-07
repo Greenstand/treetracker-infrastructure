a4f3dd6048a2d3bb09a68d51bbc78223745a2870
root <Matthew Kornfield>
2019-05-07 13:56:17 +0000

    added init and join roles for swarm

--- install/install-latest-ansible.sh
+++ install/install-latest-ansible.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository universe
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common --yes
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install ansible
-sudo apt-get install python3-pip
-pip3 install docker
+sudo apt-get install python3-pip --yes
+pip3 install ansible docker --upgrade

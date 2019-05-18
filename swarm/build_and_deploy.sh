5daf48c283b9343e3897f3fe810ecbb5f9eddc8a
Matthew Kornfield <mckornfield@gmail.com>
2019-05-17 22:43:12 -0400

    more cleanup, get errors from certs

--- swarm/build_and_deploy.sh
+++ swarm/build_and_deploy.sh
@@ -4,4 +4,4 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 
-ansible-playbook deploy-all-playbook.yml -e "version="$1 --vault-id vault-password-file -vv
+ansible-playbook build-and-deploy-all-playbook.yml -e "version="$1 --vault-id vault-password-file -vv

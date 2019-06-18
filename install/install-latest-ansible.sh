1adbdb902e3d74b668879ce6883f2ae67668ab2b
Matthew Kornfield <mckornfield@gmail.com>
2019-06-17 21:38:22 -0400

    more python3 fun

--- install/install-latest-ansible.sh
+++ install/install-latest-ansible.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo add-apt-repository universe
 sudo apt-get update
-sudo apt-get install libffi-dev libssl-dev software-properties-common --yes
-sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install libffi-dev libssl-dev software-properties-common python3-docker --yes
 sudo apt-get install python3-dev python3-pip --yes
 pip3 install ansible docker --upgrade

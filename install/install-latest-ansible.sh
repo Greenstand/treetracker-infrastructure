40bf6fef130ffec2d3d023e1e3945c62691583c2
Matthew Kornfield <mckornfield@gmail.com>
2019-06-17 22:57:04 -0400

    update panel build, still not working

--- install/install-latest-ansible.sh
+++ install/install-latest-ansible.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository universe
 sudo apt-get update
-sudo apt-get install libffi-dev libssl-dev software-properties-common python3-docker --yes
-sudo apt-get install python3-dev python3-pip --yes
+sudo apt-get install libffi-dev libssl-dev software-properties-common --yes
+sudo apt-get install node-gyp npm --yes
+sudo apt-get install python3-dev python3-pip python3-docker --yes
 pip3 install ansible docker --upgrade

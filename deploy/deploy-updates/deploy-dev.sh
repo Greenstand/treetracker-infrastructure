051d0ecd6c400ec02950eafac6c2d52313dada4e
Zaven <matthew.fullstackgis@gmail.com>
2020-02-26 16:26:22 -0800

    deployment scripts for data pipeline

--- deploy/deploy-updates/deploy-dev.sh
+++ deploy/deploy-updates/deploy-dev.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 ansible-playbook  -i ../hosts/dev.hosts -l batch --user root  playbooks/update-pipeline/update-pipeline-microservice.yml
 ansible-playbook  -i ../hosts/dev.hosts -l batch --user root  playbooks/update-pipeline/update-pipeline-consumer.yml
+ansible-playbook  -i ../hosts/dev.hosts -l batch --user root  playbooks/update-pipeline/update-pipeline-cron.yml
--- deploy/deploy-updates/package.sh
+++ deploy/deploy-updates/package.sh
@@ -12,6 +12,7 @@ find ./build/ -name *.spec.js -exec rm {} \;
 
 cp ../config/$ENV/pipeline-consumer/* build/treetracker-database/pipeline/consumer/config/
 cp ../config/$ENV/pipeline-microservice/* build/treetracker-database/pipeline/microservice/config/
+cp ../config/$ENV/pipeline-cron/* build/treetracker-database/pipeline/cron/config/
 
 cd build/treetracker-database/pipeline/consumer/
 npm ci &
@@ -19,11 +20,15 @@ cd ../../../../
 cd build/treetracker-database/pipeline/microservice/
 npm ci &
 cd ../../../../
+cd build/treetracker-database/pipeline/cron/
+npm ci &
+cd ../../../../
 
 wait
 
 tar -cvzf build/treetracker-pipeline-consumer.tar.gz --directory build/treetracker-database/pipeline/consumer/ . &
 tar -cvzf build/treetracker-pipeline-microservice.tar.gz --directory build/treetracker-database/pipeline/microservice/ . &
+tar -cvzf build/treetracker-pipeline-cron.tar.gz --directory build/treetracker-database/pipeline/cron/ . &
 
 wait
 

fe95fc4c3f4ec3322e884884907f5e92124d6cbd
Zaven <matthew.fullstackgis@gmail.com>
2021-01-25 11:29:05 -0800

    Use bigger resources for cloud services node pool, so rabbitmq default resource requests can fit

--- kubernetes/terraform/cluster.tf
+++ kubernetes/terraform/cluster.tf
@@ -1,11 +1,16 @@
 
 variable "cluster_name" {}
 
+data "digitalocean_kubernetes_versions" "treetracker_kubernetes_version" {
+  version_prefix = "1.19."
+}
+
 resource "digitalocean_kubernetes_cluster" "kubernetes-cluster" {
   name   = var.cluster_name
   region = "nyc1"
-  # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.19.3-do.3"
+  auto_upgrade = true
+  version      = data.digitalocean_kubernetes_versions.treetracker_kubernetes_version.latest_version
+
 
   node_pool {
     name       = "default-node-pool"
@@ -20,7 +25,7 @@ resource "digitalocean_kubernetes_node_pool" "cloud-services-node-pool" {
   cluster_id =  digitalocean_kubernetes_cluster.kubernetes-cluster.id
 
   name       = "cloud-services-node-pool"
-  size       = "s-2vcpu-2gb"
+  size       = "s-2vcpu-4gb"
   node_count = 3
   tags       = ["cloud-services-node"]
 

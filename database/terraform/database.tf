62c96d0ba30ba16d6e42b3d4e791043e0f78144b
Zaven <matthew.fullstackgis@gmail.com>
2021-02-03 16:20:20 -0800

    Add firewall rules and upgrade read only node

--- database/terraform/database.tf
+++ database/terraform/database.tf
@@ -29,6 +29,33 @@ resource "digitalocean_database_firewall" "treetracker-database-fw" {
     type  = "tag"
     value = "bastion"
   }
+
+  rule {
+    type  = "droplet"
+    value = "prod-core-services-20200703-01-1612148248329-s-4vcpu-8gb-nyc1-01"
+  }
+
+  rule {
+    type  = "droplet"
+    value = "prod-legacy-core-services-1612148271925-s-2vcpu-4gb-nyc1-01"
+  }
+
+  rule {
+    type  = "ip_addr"
+    value = "157.230.2.227"
+  }
+
+  rule {
+    type  = "ip_addr"
+    value = "157.230.8.187"
+  }
+
+  rule {
+    type  = "ip_addr"
+    value = "69.209.22.114"
+  }
+
+
 }
 
 
@@ -36,7 +63,7 @@ resource "digitalocean_database_firewall" "treetracker-database-fw" {
 resource "digitalocean_database_replica" "treetracker-postgres-read-replica" {
   cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
   name       = "read-replica"
-  size       = "db-s-1vcpu-1gb"
+  size       = "db-s-2vcpu-4gb"
   region     = "nyc1"
 }
 
@@ -62,60 +89,4 @@ resource "digitalocean_database_db" "data-pipeline-database" {
 }
 
 
-
-resource "digitalocean_database_user" "admin-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "u_admin"
-}
-
-resource "digitalocean_database_user" "manager-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "u_manager"
-}
-
-resource "digitalocean_database_user" "analyst-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "u_analyst"
-}
-
-resource "digitalocean_database_user" "token-issuer-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "u_token_issuer"
-}
-
-resource "digitalocean_database_user" "read-only-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "u_read_only"
-}
-
-
-resource "digitalocean_database_user" "map-service-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "s_map"
-}
-
-resource "digitalocean_database_user" "admin-panel-service-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "s_admin_panel"
-}
-
-resource "digitalocean_database_user" "data-pipeline-service-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "s_data_pipeline"
-}
-
-resource "digitalocean_database_user" "wallet-service-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "s_wallet"
-}
-
-resource "digitalocean_database_user" "field-service-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "s_field"
-}
-
-resource "digitalocean_database_user" "treetracker-service-user" {
-  cluster_id = digitalocean_database_cluster.treetracker-postgres-cluster.id
-  name       = "s_treetracker"
-}
 

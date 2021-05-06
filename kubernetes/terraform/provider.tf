87a4c3be456925b5349a0585c700caadb2d0bcc9
vinodhinivijayan <44034881+vinodhinivijayan@users.noreply.github.com>
2021-05-06 19:21:08 -0400

    Update provider.tf

--- kubernetes/terraform/provider.tf
+++ kubernetes/terraform/provider.tf
@@ -8,5 +8,17 @@ terraform {
       source = "digitalocean/digitalocean"
       version = "1.22.2"
     }
+    
   }
+}
+data "digitalocean_kubernetes_cluster" "kubernetes-cluster" {
+  name = "dev-k8s-treetracker"
+}
+
+provider "kubernetes" {
+  host             = data.digitalocean_kubernetes_cluster.kubernetes-cluster.endpoint
+  token            = data.digitalocean_kubernetes_cluster.kubernetes-cluster.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.kuberntes-cluster.kube_config[0].cluster_ca_certificate
+  )
 }

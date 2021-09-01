5d713d20bffbadf4ee72bef059bc14459d1483db
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-08-31 19:52:13 -0700

    feat: improve rabbitmq infra scripts and deployment

--- rabbitmq/scripts/get-credentials.sh
+++ rabbitmq/scripts/get-credentials.sh
@@ -1,11 +1,11 @@
-kubectl get rabbitmqcluster  rabbitmqcluster-sample \
+kubectl get rabbitmqcluster  rabbitmqcluster-main \
 -ojsonpath='Name: {.status.admin.serviceReference.name} -- Namespace: {.status.admin.serviceReference.namespace}'
 echo  ""
 echo  -n "username "
 #first get the name of default user secret
-#kubectl get rabbitmqcluster rabbitmqcluster-sample -ojsonpath='{.status.defaultUser.secretReference.name}'
-kubectl get secret rabbitmqcluster-sample-default-user -o jsonpath="{.data.username}" | base64 --decode
+#kubectl get rabbitmqcluster rabbitmqcluster-main -ojsonpath='{.status.defaultUser.secretReference.name}'
+kubectl get secret rabbitmqcluster-main-default-user -o jsonpath="{.data.username}" | base64 --decode
 echo  ""
 echo  -n "password "
-kubectl get secret rabbitmqcluster-sample-default-user -o jsonpath="{.data.password}" | base64 --decode
+kubectl get secret rabbitmqcluster-main-default-user -o jsonpath="{.data.password}" | base64 --decode
 echo  ""

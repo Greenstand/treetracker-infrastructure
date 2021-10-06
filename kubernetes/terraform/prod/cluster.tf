8058abbf899b5f896754c51ddcd89f1ee142b595
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-10-05 18:03:37 -0700

    feat: add monitoring nodes to production cluster

---
+++ kubernetes/terraform/prod/cluster.tf
@@ -0,0 +1,56 @@
+variable "cluster_name" {}
+
+data "digitalocean_kubernetes_versions" "treetracker_kubernetes_version" {
+  version_prefix = "1.19."
+}
+
+resource "digitalocean_kubernetes_cluster" "kubernetes-cluster" {
+  name   = var.cluster_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  region = "nyc1"
+  auto_upgrade = true
+  version      = data.digitalocean_kubernetes_versions.treetracker_kubernetes_version.latest_version
+
+  node_pool {
+    name       = "default-node-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 1
+    tags       = ["microservices-node", "treetracker-database-access"]
+  }
+
+}
+
+resource "digitalocean_kubernetes_node_pool" "microservices-node-pool" {
+  cluster_id =  digitalocean_kubernetes_cluster.kubernetes-cluster.id
+
+  name       = "microservices-node-pool"
+  size       = "s-2vcpu-4gb"
+  node_count = 3
+  tags       = ["microservices-node", "treetracker-database-access"]
+
+}
+
+resource "digitalocean_kubernetes_node_pool" "cloud-services-node-pool" {
+  cluster_id =  digitalocean_kubernetes_cluster.kubernetes-cluster.id
+
+  name       = "cloud-services-node-pool"
+  size       = "s-2vcpu-4gb"
+  node_count = 3
+  tags       = ["cloud-services-node", "treetracker-database-access"]
+
+}
+
+resource "digitalocean_kubernetes_node_pool" "monitoring-node-pool" {
+  cluster_id =  digitalocean_kubernetes_cluster.kubernetes-cluster.id
+
+  name       = "monitoring-node-pool"
+  size       = "s-4vcpu-8gb"
+  node_count = 3
+  tags       = ["monitoring-node"]
+
+}
+

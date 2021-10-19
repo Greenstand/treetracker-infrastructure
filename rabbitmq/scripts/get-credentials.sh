c79f8dc21b12543282f73154d1fdc67a7276a464
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-10-19 16:48:24 -0700

    chore: update rabbitmq get creds script

--- rabbitmq/scripts/get-credentials.sh
+++ rabbitmq/scripts/get-credentials.sh
@@ -1,11 +1,11 @@
-kubectl get rabbitmqcluster  rabbitmqcluster-main \
+kubectl get -n rabbitmq-cluster rabbitmqcluster  rabbitmqcluster-main \
 -ojsonpath='Name: {.status.admin.serviceReference.name} -- Namespace: {.status.admin.serviceReference.namespace}'
 echo  ""
 echo  -n "username "
 #first get the name of default user secret
 #kubectl get rabbitmqcluster rabbitmqcluster-main -ojsonpath='{.status.defaultUser.secretReference.name}'
-kubectl get secret rabbitmqcluster-main-default-user -o jsonpath="{.data.username}" | base64 --decode
+kubectl get -n rabbitmq-cluster secret rabbitmqcluster-main-default-user -o jsonpath="{.data.username}" | base64 --decode
 echo  ""
 echo  -n "password "
-kubectl get secret rabbitmqcluster-main-default-user -o jsonpath="{.data.password}" | base64 --decode
+kubectl get -n rabbitmq-cluster secret rabbitmqcluster-main-default-user -o jsonpath="{.data.password}" | base64 --decode
 echo  ""

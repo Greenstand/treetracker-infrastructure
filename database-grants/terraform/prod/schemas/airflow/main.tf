637059e13876e9e227b9e26c2a35de7b10bc39f6
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-10-20 15:08:48 -0700

    fix: tweak airflow db user priviledges

--- database-grants/terraform/prod/schemas/airflow/main.tf
+++ database-grants/terraform/prod/schemas/airflow/main.tf
@@ -1,7 +1,46 @@
+resource "postgresql_schema" "microservice_schema" {
+  name  = "airflow"
+  owner = "doadmin"
 
-module "microservice_schema" {
-  source = "./../../modules/microservice_schema"
-  schema = "airflow"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "random_password" "s_password" {
+  length = 16
+  special = true
+  override_special = "_%@"
+}
+
+resource "postgresql_role" "service_user" {
+  name     = "s_airflow"
+  login    = true
+  password = random_password.s_password.result
 }
 
 
+resource "postgresql_grant" "service-user-usage" {
+  database    = "treetracker"
+  role        = "s_airflow"
+  schema      = "airflow"
+  object_type = "schema"
+  privileges  = ["USAGE", "CREATE"]
+}
+
+resource "postgresql_grant" "table-service-user" {
+  database    = "treetracker"
+  role        = "s_airflow"
+  schema      = "airflow"
+  object_type = "table"
+  privileges  = ["SELECT", "INSERT", "UPDATE"]
+}
+
+resource "postgresql_grant" "sequence-service-user" {
+  database    = "treetracker"
+  role        = "s_airflow"
+  schema      = "airflow"
+  object_type = "sequence"
+  privileges  = ["USAGE", "SELECT"]
+
+}

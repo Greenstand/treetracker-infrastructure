c09fd2e3153176fd0f4a8adb59b158c85bb052e7
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-12-14 18:19:49 -0800

    Allow push to wallet dev cdn

--- aws_access/terraform/users.tf
+++ aws_access/terraform/users.tf
@@ -74,6 +74,16 @@ resource "aws_iam_user_policy" "cloudfront_user_policy" {
       {
          "Effect":"Allow",
          "Action": "s3:*Object",
+         "Resource":"arn:aws:s3:::dev.wallet.treetracker.org/*"
+      },
+      {
+         "Effect": "Allow",
+         "Action": "s3:ListBucket",
+         "Resource": "arn:aws:s3:::dev.wallet.treetracker.org"
+      },
+      {
+         "Effect":"Allow",
+         "Action": "s3:*Object",
          "Resource":"arn:aws:s3:::test.webmap.treetracker.org/*"
       },
       {

c4b49e1c473f51ef33a48580c25b0c4f6f535870
Zaven Arra <matthew.fullstackgis@gmail.com>
2022-05-17 17:53:31 -0700

    Add staging web map cdn

---
+++ web-apps/terraform/staging-web-map.tf
@@ -0,0 +1,26 @@
+module "staging-web-map" {
+  source  = "github.com/Greenstand/terraform-aws-staticwebsite"
+  domain = "staging-map.treetracker.org"
+  error_document = "index.html"
+  routing_rules = <<EOF
+[{
+  "Condition" : {
+    "KeyPrefixEquals" : "finorx-"
+  },
+  "Redirect"  : {
+    "HostName"             : "staging-map.treetracker.org",
+      "ReplaceKeyPrefixWith" : "?tree_name="
+  }
+},
+{
+  "Condition" : {
+    "KeyPrefixEquals" : "tree/"
+  },
+  "Redirect"  : {
+    "HostName"             : "staging-map.treetracker.org",
+      "ReplaceKeyPrefixWith" : "?tree_name="
+  }
+}]
+EOF
+
+}

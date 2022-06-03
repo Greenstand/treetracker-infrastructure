9c1fd95f6811e5a9a6a479b436563607b691ad1b
ximena9201 <ximenandrea.ro@gmail.com>
2022-06-03 15:14:16 -0400

    disable ipv6

--- aws_access/terraform/cdn.tf
+++ aws_access/terraform/cdn.tf
@@ -26,7 +26,7 @@ resource "aws_cloudfront_distribution" "default" {
   }
 
   enabled             = true
-  is_ipv6_enabled     = true
+  is_ipv6_enabled     = false
   default_root_object = "index.html"
 
   viewer_certificate {

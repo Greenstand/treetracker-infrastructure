2e53eb37cc2a490d90f2884261a85a84242d422a
ximena9201 <ximenandrea.ro@gmail.com>
2022-06-03 14:11:58 -0400

    add sample TF stack for CDN

---
+++ aws_access/terraform/cdn.tf
@@ -0,0 +1,42 @@
+data "aws_s3_bucket" "default" {
+  bucket = "treetracker-dev-images"
+}
+
+resource "aws_s3_bucket_acl" "default" {
+  bucket = aws_s3_bucket.default.id
+  acl    = "public-read"
+}
+
+resource "aws_cloudfront_distribution" "default" {
+  origin {
+    domain_name = aws_s3_bucket.default.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.default.bucket_regional_domain_name
+  }
+
+  origin {
+    domain_name = aws_lb.default.dns_name
+    origin_id   = aws_lb.default.dns_name
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["TLSv1.2"]
+      origin_protocol_policy = "http-only"
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.default.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}

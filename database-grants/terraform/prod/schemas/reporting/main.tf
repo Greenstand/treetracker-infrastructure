dee445cf955dccb1502dbaaccaf5ca21c30ee875
Daniel Olojakpoke <holodja@hotmail.com>
2022-10-23 23:08:22 +0100

    fix: give reporting access to stakeholder schema

--- database-grants/terraform/prod/schemas/reporting/main.tf
+++ database-grants/terraform/prod/schemas/reporting/main.tf
@@ -5,4 +5,31 @@ module "microservice_schema" {
   service_user_table_grants = ["SELECT", "INSERT", "UPDATE", "DELETE"] # allow delete from this schema
 }
 
+resource "postgresql_grant" "stakeholder_schema" {
+  database    = "treetracker"
+  role        = "s_reporting"
+  schema      = "stakeholder"
+  object_type = "schema"
+  privileges  = ["USAGE"]
+}
+
+resource "postgresql_grant" "stakeholder_children_function" {
+  database    = "treetracker"
+  role        = "s_reporting"
+  schema      = "stakeholder"
+  object_type = "function"
+  privileges  = ["EXECUTE"]
+}
+
+resource "postgresql_grant" "stakeholder_table" {
+  database    = "treetracker"
+  role        = "s_reporting"
+  schema      = "stakeholder"
+  object_type = "table"
+  privileges  = ["SELECT"]
+}
+
+
+
+
 

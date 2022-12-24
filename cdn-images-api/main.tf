ad7cc8e83dddef82dbf5b1e97c2293d224dbc7ec
emmanue <brinkcorp@gmail.com>
2022-12-24 23:34:50 +0000

    add images api cdn for dev env

--- cdn-images-api/main.tf
+++ cdn-images-api/main.tf
@@ -1,15 +1,16 @@
 resource "aws_cloudfront_distribution" "images_cdn" {
-  optimizer {
-    origin {
-        domain_name = images-api.treetracker.org
-        custom_origin_config {
-        http_port = "80"
-        https_port = "443"
+  origin {
+    optimizer{
+      domain_name = "images-api.treetracker.org"
+      custom_origin_config {
+        http_port              = "80"
+        https_port             = "443"
         origin_protocol_policy = "http-only"
-        origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-        }
-    }
+        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      }
+    } 
   }
+  
   
   enabled             = true
   is_ipv6_enabled     = true
@@ -22,10 +23,10 @@ resource "aws_cloudfront_distribution" "images_cdn" {
     target_origin_id       = "optimizer"
     viewer_protocol_policy = "allow-all"
 
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    compress         = true
-    query_string     = true
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    query_string           = true
 
     # Removed min_ttl default_ttl and max_ttl to enable use origin cache headers
     
@@ -33,14 +34,14 @@ resource "aws_cloudfront_distribution" "images_cdn" {
 
   # Cache behavior with precedence 0
   ordered_cache_behavior {
-    path_pattern     = "/*"
-    target_origin_id = "optimizer"
+    path_pattern           = "/*"
+    target_origin_id       = "optimizer"
 
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
     
-    compress        = true
-    query_string    = true
+    compress               = true
+    query_string           = true
     viewer_protocol_policy = "redirect-to-https"
   }
 

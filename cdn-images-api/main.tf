deeac789f86c4856d22e4f1eb77e5b12269ba4c9
emmanue <brinkcorp@gmail.com>
2022-12-24 23:52:14 +0000

    add images api cdn for dev env

--- cdn-images-api/main.tf
+++ cdn-images-api/main.tf
@@ -12,33 +12,30 @@ resource "aws_cloudfront_distribution" "images_cdn" {
   }
   
   
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Some comment"
-  default_root_object = "index.html"
 
-
+  enabled         = true
+  is_ipv6_enabled = true
+  
 
   default_cache_behavior {
     target_origin_id       = "optimizer"
     viewer_protocol_policy = "allow-all"
 
-    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods         = ["GET", "HEAD"]
-    compress               = true
-    query_string           = true
+    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+    query_string    = true
 
-    # Removed min_ttl default_ttl and max_ttl to enable use origin cache headers
-    
+    # Removed min_ttl default_ttl and max_ttl to enable use origin cache headers 
   }
 
   # Cache behavior with precedence 0
   ordered_cache_behavior {
-    path_pattern           = "/*"
-    target_origin_id       = "optimizer"
+    path_pattern     = "/*"
+    target_origin_id = "optimizer"
 
-    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
-    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD", "OPTIONS"]
     
     compress               = true
     query_string           = true

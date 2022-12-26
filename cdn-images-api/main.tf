130216d01559a8ae749fd7d305190fe9c008d9dc
emmanue <brinkcorp@gmail.com>
2022-12-26 09:51:21 +0000

    feat: add cdn for images api

--- cdn-images-api/main.tf
+++ cdn-images-api/main.tf
@@ -1,16 +1,16 @@
 resource "aws_cloudfront_distribution" "images_cdn" {
   optimizer {
     origin {
-        domain_name = images-api.treetracker.org
-        custom_origin_config {
-        http_port = "80"
-        https_port = "443"
+      domain_name = images-api.treetracker.org
+      custom_origin_config {
+        http_port              = "80"
+        https_port             = "443"
         origin_protocol_policy = "http-only"
-        origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-        }
+        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      }
     }
   }
-  
+
 
   enabled             = true
   is_ipv6_enabled     = true
@@ -23,13 +23,13 @@ resource "aws_cloudfront_distribution" "images_cdn" {
     target_origin_id       = "optimizer"
     viewer_protocol_policy = "allow-all"
 
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    compress         = true
-    query_string     = true
+    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+    query_string    = true
 
     # Removed min_ttl default_ttl and max_ttl to enable use origin cache headers
-    
+
   }
 
   # Cache behavior with precedence 0
@@ -37,11 +37,11 @@ resource "aws_cloudfront_distribution" "images_cdn" {
     path_pattern     = "/*"
     target_origin_id = "optimizer"
 
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    
-    compress        = true
-    query_string    = true
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD", "OPTIONS"]
+
+    compress               = true
+    query_string           = true
     viewer_protocol_policy = "redirect-to-https"
   }
 

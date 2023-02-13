e43116a0c78140c813149859e165cbaa165390e8
Jon Faviell <jon.faviell@gmail.com>
2023-02-13 15:29:52 -0600

    copy prod main.tf to test

---
+++ database-grants/terraform/test/main.tf
@@ -0,0 +1,85 @@
+
+module "field_data_schema" {
+  source = "./schemas/field_data"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+
+module "wallet_schema" {
+  source = "./schemas/wallet"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "treetracker_schema" {
+  source = "./schemas/treetracker"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "earnings_schema" {
+  source = "./schemas/earnings"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "webmap_schema" {
+  source = "./schemas/webmap"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "reporting_schema" {
+  source = "./schemas/reporting"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "airflow_schema" {
+  source = "./schemas/airflow"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "messaging_schema" {
+  source = "./schemas/messaging"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "query_schema" {
+  source = "./schemas/query"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "stakeholder_schema" {
+  source = "./schemas/stakeholder"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "regions_schema" {
+  source = "./schemas/regions"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}
+
+module "contracts_schema" {
+  source = "./schemas/contracts"
+  providers = {
+    postgresql = postgresql.treetracker
+  }
+}

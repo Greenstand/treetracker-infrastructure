bfa838d5b8acccde9dd15c240cb778ba29e9fe39
dadiorchen <antlet2004@gmail.com>
2023-10-21 13:02:28 +0800

    feat: text on the bottom

--- keycloak/themes/treetracker/login/resources/js/treetracker.js
+++ keycloak/themes/treetracker/login/resources/js/treetracker.js
@@ -1,1 +1,18 @@
-alert("Hello World!");
+// javascript to append a new div to body
+var newDiv = document.createElement("div");
+newDiv.innerHTML = `
+<div style="
+    position: fixed;
+    bottom: 0px;
+    margin: 10px 20px;
+    background-color: #09090980;
+    color: white;
+    font-size: 16px;
+    padding: 5px;
+">On the background, it is one tree on the system, which is a mangrove capable of trapping a ton of carbon - if it can survive a city where some 800 thousand people rely on wood to cook; The chances that it will survive are drastically increased by payments made to the grower who guards it. Our work is creating green employment for people, check <a href="https://map.treetracker.org/?treeid=5457697">this tree</a> on our map.</div>
+`;
+
+// Append new div to body when the DOM is loaded
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.body.appendChild(newDiv);
+});

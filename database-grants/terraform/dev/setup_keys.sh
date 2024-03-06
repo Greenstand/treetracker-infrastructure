84c0fb150b04196d9c22159794125dbd7890e24c
sanket.nadkarni <sanket.nadkarni@alpha-sense.com>
2024-03-06 22:34:21 +0530

    fix: revert the script to previous commit

--- database-grants/terraform/dev/setup_keys.sh
+++ database-grants/terraform/dev/setup_keys.sh
@@ -3,9 +3,6 @@
 # Retrieve keys from your netrc, specified as
 # machine sfo2.digitaloceanspaces.com login KEY password SECRET
 # Source this file before running terraform commands
-
-grep_string=$(cat backend.tf | grep endpoint | awk -F'=' '{print $2}' | awk -F'/' '{print $NF}' | sed 's/"//g')
-
-netrc_string=$(grep ${grep_string} ~/.netrc)
+netrc_string=$(grep digitalocean.dev.treetracker.org ~/.netrc)
 export AWS_ACCESS_KEY_ID=$(echo $netrc_string | awk '{print $4}')
 export AWS_SECRET_ACCESS_KEY=$(echo $netrc_string | awk '{print $6}')

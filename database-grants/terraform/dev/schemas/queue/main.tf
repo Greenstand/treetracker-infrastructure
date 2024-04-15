152fa82425b261805a626d79fd3399ae8e83bf0d
partha04patel <parth04patel@gmail.com>
2024-04-15 17:03:26 -0400

    add users and roles for access to queue schema in dev

--- database-grants/terraform/dev/schemas/queue/main.tf
+++ database-grants/terraform/dev/schemas/queue/main.tf
@@ -3,3 +3,42 @@ module "microservice_schema" {
   schema = "queue"
   service_user_table_grants = ["SELECT", "INSERT", "UPDATE", "DELETE"]
 }
+
+resource "random_password" "s_password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+resource "postgresql_role" "service_user" {
+  name        = "s_queue"
+  login       = true
+  password    = random_password.s_password.result
+  search_path = ["queue"]
+}
+
+
+resource "postgresql_grant" "service-user-usage" {
+  database    = "treetracker"
+  role        = "s_queue"
+  schema      = "queue"
+  object_type = "schema"
+  privileges  = ["USAGE", "CREATE"]
+}
+
+resource "postgresql_grant" "table-service-user" {
+  database    = "treetracker"
+  role        = "s_queue"
+  schema      = "queue"
+  object_type = "table"
+  privileges  = ["SELECT", "INSERT", "UPDATE", "DELETE"]
+}
+
+resource "postgresql_grant" "sequence-service-user" {
+  database    = "treetracker"
+  role        = "s_queue"
+  schema      = "queue"
+  object_type = "sequence"
+  privileges  = ["USAGE", "SELECT"]
+
+}
